960d1abf0d983e825dc875487564658b885ccaa5
John Bonini <jbonini@flatironinstitute.org>
2023-03-08 12:37:15 -0800

    func to set org latex size

--- config.el
+++ config.el
@@ -159,8 +159,13 @@ otherwise use the subtree title."
 
 
   ;; for inline latex
-  (plist-put org-format-latex-options :scale 2)
   (setq org-latex-packages-alist '(("" "braket" t) ("" "amsmath" t) ("bb=dsserif" "mathalpha" t)))
+  (plist-put org-format-latex-options :scale 2)
+  (defun jrb/org-scale-latex (&optional arg)
+    (interactive "P")
+    (let ((scale  (if arg (prefix-numeric-value arg) 2)))
+      (message "latex scale set to %s" scale)
+      (plist-put org-format-latex-options :scale scale)))
 
   (remove-hook! 'org-mode-hook #'+org|enable-auto-update-cookies)
   (advice-remove #'evil-org-open-below #'+org*evil-org-open-below) ; didn't like this anyway

67f0a7609914ba7bc203c1348498171976826e9d
John Bonini <jbonini@flatironinstitute.org>
2023-05-14 15:32:48 -0400

    tree sitter on mac (emacs 29)

--- init.el
+++ init.el
@@ -118,6 +118,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
+       (:if IS-MAC tree-sitter)       ; (until other systems go to v29) syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os

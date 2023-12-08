767b0528f9e72c2e8c70bc9e672c3677f3552623
John Bonini <bonini.physics@gmail.com>
2023-12-08 15:35:16 -0500

    set julia things slightly better

--- config.el
+++ config.el
@@ -10,9 +10,9 @@
 ;; disable doom splash image
 (setq +doom-dashboard-functions (cdr +doom-dashboard-functions))
 
+(setq lsp-julia-package-dir nil)
 (after! lsp-julia
-  (setq lsp-julia-default-environment "~/.julia/environments/v1.7")
-  (setq lsp-julia-package-dir nil))
+  (setq lsp-julia-default-environment "~/.julia/environments/v1.9"))
 
 (after! (:and julia-repl inheritenv)
   (inheritenv-add-advice 'julia-repl-inferior-buffer))

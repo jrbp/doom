b60ebdcb0dc2049869c0c320f26d148ff3461b3e
John Bonini <bonini.physics@gmail.com>
2024-03-19 18:39:18 -0400

    gemeni

--- modules/private/gptel/config.el
+++ modules/private/gptel/config.el
@@ -4,4 +4,9 @@
   :config
   (let ((file-name-handler-alist '(("\\.gpg\\(~\\|\\.~[0-9]+~\\)?\\'" . epa-file-handler))))
     (load-file (expand-file-name "modules/private/gptel/secrets/apikeys.el.gpg" doom-user-dir)))
-  (setq! gptel-api-key (alist-get 'openai llm-apikey-alist)))
+  (setq! gptel-default-mode 'org-mode)
+  (setq! gptel-api-key (alist-get 'openai llm-apikey-alist))
+  (set-popup-rules!
+    '(("^\\*ChatGPT.*" :ignore t)))
+  (gptel-make-gemini "Gemini" :key (alist-get 'gemini llm-apikey-alist) :stream t)
+  )

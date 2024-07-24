5a6ae9bf95ab0789ac1a38d623ec5ae795deed2e
John Bonini <bonini.physics@gmail.com>
2024-07-24 11:05:40 -0400

    remove unused custom from config
    doom does it's own custom.el (that I also don't use)

--- config.el
+++ config.el
@@ -482,18 +482,3 @@ jupyter kernels after pyenv env is changed"
   (mapc #'call-interactively '(save-buffer org-edit-src-exit)))
 (advice-add 'evil-org-edit-src-exit :override
             'replace-evil-org-edit-src-exit)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((org-ref-pdf-directory . "/Documents/papers/pto_divacancies/")
-     (org-ref-pdf-directory . "~/Documents/papers/wannier_pol/"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

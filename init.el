ec16b790549f039d3b9e51cc8811cd54662ec6c5
John Bonini <bonini.physics@gmail.com>
2024-10-09 22:46:43 -0400

    enable rust + lsp

--- init.el
+++ init.el
@@ -212,7 +212,9 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust
+        +lsp
+        )              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        ;;(sh +fish)        ; she sells (ba|z|fi)sh shells on the C xor

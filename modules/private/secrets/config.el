d155da398367542fdcb0ee900ac4dce3588da178
John Bonini <bonini.physics@gmail.com>
2024-12-28 18:51:37 -0500

    use loopback gpg pinentry

--- modules/private/secrets/config.el
+++ modules/private/secrets/config.el
@@ -1,4 +1,5 @@
 ;;; private/secrets/config.el -*- lexical-binding: t; -*-
 
+(setq! epg-pinentry-mode 'loopback)
 (let ((file-name-handler-alist '(("\\.gpg\\(~\\|\\.~[0-9]+~\\)?\\'" . epa-file-handler))))
   (load-file (expand-file-name "modules/private/secrets/secrets.el.gpg" doom-user-dir)))

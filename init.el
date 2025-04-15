baa26897767c7801ed03645d2d3ccbb2503e183b
John Bonini <bonini.physics@gmail.com>
2025-04-14 21:17:39 -0400

    company -> corfu

--- init.el
+++ init.el
@@ -23,11 +23,11 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company          ; the ultimate code completion backend
-         ;+childframe
-         )
+       ;; (company          ; the ultimate code completion backend
+       ;;   ;+childframe
+       ;;   )
         ;)
-       ;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
+       (corfu +orderless +icons)  ; complete with cap(f), cape and a flying feather!
        ;(helm             ; the *other* search engine for love and life
        ; +fuzzy)          ; enable fuzzy search backend for helm
        ;;ido               ; the other *other* search engine...

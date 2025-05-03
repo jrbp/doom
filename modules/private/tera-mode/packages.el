27cabfdc87f89cac9e68171c4308f511dc595a07
John Bonini <bonini.physics@gmail.com>
2025-05-03 11:02:59 -0400

    pin terra mode

--- modules/private/tera-mode/packages.el
+++ modules/private/tera-mode/packages.el
@@ -1,3 +1,3 @@
 ;;; private/tera-mode/packages.el
 (package! tera-mode
-  :recipe (:host github :repo "svavs/tera-mode"))
+  :recipe (:host github :repo "svavs/tera-mode") :pin "cf3e9a2d86853eb2162b8c2160cdf57d7ca6a442")

bf77d714a9066bf40325592a752b8b014eb467df
John Bonini <bonini.physics@gmail.com>
2025-05-03 11:48:50 -0400

    trying something for ox-zola + unstraightened

--- modules/private/ox-zola/packages.el
+++ modules/private/ox-zola/packages.el
@@ -2,4 +2,4 @@
 ;; requires ox-hugo (enabled with org +hugo in init.el)
 ;;(package! ox-zola :recipe (:host github :repo "gicrisf/ox-zola"))
 ;; using my fork
-(package! ox-zola :recipe (:host github :repo "jrbp/ox-zola") :pin "fb5afbc203125ae9a53a516be1924da2a0838543")
+(package! ox-zola :recipe (:host github :repo "jrbp/ox-zola" :includes ox-hugo) :pin "fb5afbc203125ae9a53a516be1924da2a0838543")

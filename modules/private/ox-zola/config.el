a09eacfc2745d20df428623b2c02171b8143308e
John Bonini <bonini.physics@gmail.com>
2025-05-03 12:16:50 -0400

    idk

--- modules/private/ox-zola/config.el
+++ modules/private/ox-zola/config.el
@@ -1,6 +1,3 @@
 ;;; private/ox-zola/config.el -*- lexical-binding: t; -*-
 (use-package! ox-zola
-  :derfer-incrementally ox-hugo
-  :after (ox)
-  :config
-  (require 'ox-hugo))
+  :after (ox))
--- modules/private/ox-zola/packages.el
+++ modules/private/ox-zola/packages.el
@@ -6,5 +6,5 @@
     :recipe (:host github :repo "kaushalmodi/ox-hugo" :nonrecursive t)
     :pin "e3365cb4e65c1853d8838b863a21546bbd9e0990")
 (package! ox-zola
-  :recipe (:host github :repo "jrbp/ox-zola" :includes ox-hugo)
+  :recipe (:host github :repo "jrbp/ox-zola" :nonrecursive t)
   :pin "fb5afbc203125ae9a53a516be1924da2a0838543")

bc8082a0294ebd07c065c4666808486826eea6e1
John Bonini <bonini.physics@gmail.com>
2026-06-23 16:38:27 -0400

    ghostel keybinds

--- modules/private/ghostel/config.el
+++ modules/private/ghostel/config.el
@@ -7,17 +7,21 @@
     (if method
         (setf method 'login-shell)     
       (push (list "rpc" 'login-shell) ghostel-tramp-shells)))
-  (map! :map evil-ghostel-mode-map
-        :localleader
-        "m" #'ghostel-semi-char-mode
-        "M" #'ghostel-char-mode
-        "e" #'ghostel-emacs-mode
-        "E" #'ghostel-copy-mode
-        "l" #'ghostel-line-mode))
+  (map! :leader :prefix-map ("o" . "open")
+        :desc "ghostel term" "g" #'ghostel))
 
 (use-package evil-ghostel
   :after (ghostel evil)
-  :hook (ghostel-mode . evil-ghostel-mode))
+  :hook (ghostel-mode . evil-ghostel-mode)
+  :config
+  (map! (:map evil-ghostel-mode-map
+         :localleader
+         "m" #'ghostel-semi-char-mode
+         "M" #'ghostel-char-mode
+         "e" #'ghostel-emacs-mode
+         "E" #'ghostel-copy-mode
+         "l" #'ghostel-line-mode)))
+
 ;; (use-package ghostel-eshell
 ;;   :after (ghostel)
 ;;   :hook (eshell-load . ghostel-eshell-visual-command-mode))
